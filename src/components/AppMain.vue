3a6b32d208eb136446d779dfadc59135c975f79c
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-03 18:22:32 +0200

    feat: cards completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,15 @@
 <script>
 import { store } from '../store';
-
 import axios from 'axios';
+
+import MediaCard from './MediaCard.vue';
 
 export default {
     name: 'AppMain',
+
+    components: {
+        MediaCard
+    },
 
     data() {
         return {
@@ -35,14 +40,22 @@ export default {
         getMediaList(apiStringToCall, category) {
             axios.get(apiStringToCall).then(callReturn => {
                 category.mediaList = callReturn.data.results;
-                console.log(category.mediaList);
+                category.page = 0;
             }).catch(error => {
                 store.getMediaListError = true;
             });
+        },
+
+        getCurrentShownMedias(category) {
+            const list = category.mediaList;
+            let firstMediaInCurrentPageIndex = category.page * 5;
+            let firstMediaInNextPageIndex = (category.page + 1) * 5 < list.length ? (category.page + 1) * 5 : undefined;
+
+            return list.slice(firstMediaInCurrentPageIndex, firstMediaInNextPageIndex);
         }
     },
 
-    mounted() {
+    created() {
         this.categories.forEach(category => {
             this.getMediaList(`${store.apiURL}${category.apiString}${store.apiKeyString}`, category);
         });
@@ -54,20 +67,86 @@ export default {
     <main>
         <template v-if="store.searchResult.length">
             <ul class="search-results media-list">
-                <li class="card" v-for="media in store.searchResult" :key="media.id"></li>
+                <li class="card" v-for="media in store.searchResult" :key="media.id">
+                    <MediaCard :media="media" />
+                </li>
             </ul>
         </template>
-        <template v-else>
+        <template v-else-if="categories[categories.length - 1]">
             <div class="category" v-for="(category, index) in categories" :key="index">
                 <h2>
                     {{ category.title }}
                 </h2>
                 <ul class="media-list">
-                    <li class="card" v-for="media in category.mediaList" :key="media.id"></li>
+                    <li class="card" v-for="media in getCurrentShownMedias(category)" :key="media.id">
+                        <MediaCard :media="media" />
+                    </li>
                 </ul>
+            </div>
+        </template>
+        <template v-else>
+            <div class="loading-screen">
+                <img src="../assets/logo.png" alt="">
             </div>
         </template>
     </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+    padding: 0 5%;
+
+    .category {
+        margin-bottom: 2rem;
+    }
+
+    h2 {
+        color: $clr-neutral-ltr;
+        font-size: 1.75vw;
+        margin-bottom: .5rem;
+    }
+
+    .media-list {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+
+        &:not(.search-results) {
+            flex-wrap: nowrap;
+        }
+
+        .card {
+            flex: 0 0 calc((100% - 4rem) / 5);
+        }
+    }
+
+    .loading-screen {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50vw;
+        max-width: 1000px;
+
+        img {
+            width: 100%;
+            animation: glowing 5s infinite;
+        }
+    }
+}
+
+@keyframes glowing {
+    0% {
+        filter: drop-shadow(0 0 10px $clr-brand-primary);
+    }
+
+    50% {
+        filter: drop-shadow(0 0 0 $clr-brand-primary);
+    }
+
+    100% {
+        filter: drop-shadow(0 0 10px $clr-brand-primary);
+    }
+}
+</style>
---
+++ src/components/MediaCard.vue
@@ -0,0 +1,152 @@
+<script>
+export default {
+    name: 'MediaCard',
+
+    props: {
+        media: Object
+    },
+
+    methods: {
+        printVote() {
+            const voteArray = []
+
+            for (let charPrinted = 0, vote = this.media.vote_average; charPrinted < 5; charPrinted++) {
+                if (vote > 1.5) {
+                    voteArray.push(2);
+                    vote = vote - 2;
+                } else if (vote > .5) {
+                    voteArray.push(1);
+                    vote--;
+                } else {
+                    voteArray.push(0);
+                }
+            }
+
+            return voteArray;
+        }
+    },
+
+    computed: {
+        getTitle() {
+            if (media.media_type === 'movie') {
+                return media.title;
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <article class="media-container">
+        <figure class="media-img">
+            <img :src="'https://image.tmdb.org/t/p/w500/' + media.poster_path" :alt="media.original_name">
+        </figure>
+
+        <div class="media-infos">
+            <div class="names-infos">
+                <strong class="name">
+                    {{ media.name }}
+                    {{ media.title }}
+                </strong>
+
+                <span class="name">
+                    {{ media.original_name }}
+                    {{ media.original_title }}
+                </span>
+            </div>
+
+            <div class="other-infos">
+                <div class="vote icon-container">
+                    <img :src="`src/assets/vote-icons/star-${voteSection}.png`"
+                        v-for="(voteSection, index) in this.printVote()" :key="index">
+                </div>
+
+                <div class="country-and-lang">
+                    <div class="language icon-container">
+                        <img alt="media.original_language"
+                            :src="`https://unpkg.com/language-icons/icons/${media.original_language}.svg`">
+                    </div>
+
+                    <div class="country icon-container">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</template>
+
+<style lang="scss" scoped>
+.media-container {
+    width: 100%;
+    border-radius: .5rem;
+    overflow: hidden;
+    background-color: $clr-neutral-dkr;
+    position: relative;
+    font-size: 1.25vw;
+    color: $clr-neutral-ltr;
+    transition: all .25s;
+
+    .media-img {
+        width: 100%;
+        aspect-ratio: 2 / 3;
+        background-image: url(../assets/placeholder-poster.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        overflow: hidden;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    &:hover {
+        transform: scale(1.25);
+        z-index: 5;
+
+        .media-img {
+            filter: brightness(50%);
+        }
+
+        .media-infos {
+            display: flex;
+        }
+    }
+
+    .media-infos {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 1vw;
+        display: none;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .names-infos {
+            .name {
+                display: block;
+                margin-bottom: 1vw;
+            }
+        }
+
+        .other-infos {
+            .icon-container {
+                height: 1.25vw;
+
+                img {
+                    height: 100%;
+                }
+            }
+
+            .country-and-lang {
+                display: flex;
+                justify-content: space-between;
+                margin-top: 1vw;
+            }
+        }
+    }
+}
+</style>
